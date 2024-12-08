f180425fb869c15a5a067a08a5c16160c7c14358
Tanmay Deshpande <dsp.tanmay@gmail.com>
2024-12-08 22:37:43 +0530

    removed unused imports and parameters

--- src/components/page-header.tsx
+++ src/components/page-header.tsx
@@ -4,23 +4,26 @@ import { ConnectButton } from "thirdweb/react";
 import { client, wallets } from "@/app/client";
 import { SignOutButton, useUser, SignInButton } from "@clerk/nextjs";
 import { Button } from "./ui/button";
+import { usePathname } from "next/navigation";
 
-function PageHeader({
-  title,
-}: {
-  title: string;
-  buttonText?: string;
-  buttonUrl?: string;
-}) {
+function PageHeader({ title }: { title: string }) {
   const { user } = useUser();
-
+  const pathName = usePathname();
   return (
     <div className="flex w-full items-center justify-between rounded-none border-b-[3px] border-border bg-bg px-8 py-5 ">
       <div className="flex items-center space-x-3">
         <h1 className="text-2xl font-bold">{title}</h1>
       </div>
       <div className="flex items-center justify-between space-x-3">
-        <ConnectButton client={client} wallets={wallets} />
+        {pathName.endsWith("/dashboard") ? (
+          <></>
+        ) : (
+          <ConnectButton
+            client={client}
+            wallets={wallets}
+            connectModal={{ size: "compact", showThirdwebBranding: false }}
+          />
+        )}
         {!user && (
           <SignInButton forceRedirectUrl={"/dashboard"}>
             <Button>Sign In</Button>
