a2305ca8b03034865b10922376bbd9e29fc3a118
Tanmay Deshpande <dsp.tanmay@gmail.com>
2024-12-08 23:07:25 +0530

    built records page with sample data

--- src/app/(user)/records/page.tsx
+++ src/app/(user)/records/page.tsx
@@ -1,11 +1,72 @@
 "use client";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 import { Wallet } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 import { useActiveAccount } from "thirdweb/react";
 
 function RecordsPageContent() {
   const activeAccount = useActiveAccount();
+  const data = [
+    {
+      token_id: 1,
+      title: "Title",
+      description: "description",
+    },
+    {
+      token_id: 1,
+      title: "Title",
+      description: "description",
+    },
+    {
+      token_id: 1,
+      title: "Title",
+      description: "description",
+    },
+    {
+      token_id: 1,
+      title: "Title",
+      description: "description",
+    },
+    {
+      token_id: 1,
+      title: "Title",
+      description: "description",
+    },
+    {
+      token_id: 1,
+      title: "Title",
+      description: "description",
+    },
+    {
+      token_id: 1,
+      title: "Title",
+      description: "description",
+    },
+    {
+      token_id: 1,
+      title: "Title",
+      description: "description",
+    },
+    {
+      token_id: 1,
+      title: "Title",
+      description: "description",
+    },
+    {
+      token_id: 1,
+      title: "Title",
+      description: "description",
+    },
+  ];
   if (!activeAccount)
     return (
       <div>
@@ -16,7 +77,32 @@ function RecordsPageContent() {
         </Alert>
       </div>
     );
-  return <div>RecordsPageContent</div>;
+  return (
+    <div className="flex w-full max-w-6xl flex-col space-y-5 rounded-none border-2 border-border bg-white px-10 py-10 shadow-light ">
+      <h2>
+        NFTs owned by{" "}
+        <span>
+          <code>{`${activeAccount.address.substring(0, 6)}...${activeAccount.address.substring(38)}`}</code>
+        </span>{" "}
+        :
+      </h2>
+      <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
+        {data.map((record, i) => (
+          <Card key={i}>
+            <CardHeader>
+              <CardTitle>{record.title}</CardTitle>
+              <CardDescription>{record.description}</CardDescription>
+            </CardHeader>
+            <CardContent>
+              <Link href={`/view/${record.token_id}`}>
+                <Button>View Document</Button>
+              </Link>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default RecordsPageContent;
