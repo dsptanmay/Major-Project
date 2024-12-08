27b212201b65510a862f99a1fcaf035ad18f7a1f
Tanmay Deshpande <dsp.tanmay@gmail.com>
2024-12-08 14:43:04 +0530

    added features page to be shown on landing page

---
+++ src/sections/features.tsx
@@ -0,0 +1,47 @@
+import {
+  Card,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import React from "react";
+
+function Features() {
+  const features = [
+    {
+      title: "Secure Ownership",
+      description:
+        "Patient health records are tokenized as NFTs, ensuring true ownership and control",
+    },
+    {
+      title: "Immutable Records",
+      description:
+        "Blockchain technology ensures records cannot be altered or tampered with",
+    },
+    {
+      title: "Easy Sharing",
+      description:
+        "Securely share your health records with healthcare providers instantly",
+    },
+  ];
+  return (
+    <div className="flex min-h-[60dvh] w-full flex-grow flex-col items-center justify-center space-y-20 border-t-[3px] border-t-border bg-white bg-[radial-gradient(#80808080_1px,transparent_1px)] px-10 py-20 [background-size:16px_16px] ">
+      <h1 className="text-4xl font-bold">Features</h1>
+      <div className="flex space-x-10">
+        {features.map((feature, i) => (
+          <Card
+            className="w-[350px] -translate-x-1 -translate-y-1 transition-all duration-200 hover:translate-x-0 hover:translate-y-0 hover:shadow-none"
+            key={i}
+          >
+            <CardHeader>
+              <CardTitle>{feature.title}</CardTitle>
+              <CardDescription>{feature.description}</CardDescription>
+            </CardHeader>
+          </Card>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Features;
