dfd54ceabf2e484b23a9733af713f5e348f02554
Tanmay Deshpande <dsp.tanmay@gmail.com>
2024-12-09 01:16:20 +0530

    updated organization dashboard page to reflect new design

--- src/components/org-dashboard.tsx
+++ src/components/org-dashboard.tsx
@@ -3,48 +3,71 @@ import PageHeader from "@/components/page-header";
 import PageFooter from "@/components/page-footer";
 import React from "react";
 import { useUser } from "@clerk/nextjs";
+import Link from "next/link";
+import {
+  Eye,
+  FileStack,
+  FolderCog,
+  MessageSquareDot,
+} from "lucide-react";
 
 function OrganizationDashboardPage() {
   const { user } = useUser();
+
+  const cards = [
+    {
+      title: "View Granted Records",
+      description: "See the records to which you have been granted access",
+      url: "/granted-records",
+      icon: <Eye />,
+    },
+    {
+      title: "Request Access",
+      description: "Request access to a particular record",
+      url: "/request-access",
+      icon: <FileStack />,
+    },
+    {
+      title: "Notifications",
+      description:
+          "See the list of pending requests for access to documents",
+      url: "/org_notifications",
+      icon: <MessageSquareDot />,
+    },
+  ];
   return (
-    <div className="inset-0 flex min-h-screen w-full flex-col items-center justify-between space-y-10 bg-white bg-[linear-gradient(to_right,#80808033_1px,transparent_1px),linear-gradient(to_bottom,#80808033_1px,transparent_1px)] bg-[size:70px_70px]">
-      <PageHeader title="Dashboard" />
-      <div className="flex w-full flex-grow p-5">
-        <div className="flex w-full flex-grow flex-col space-y-5 border-2 border-black bg-white p-5">
-          <h2 className="text-gray-700">
-            Welcome, <span className="font-semibold">{user?.username}</span>!
-            <br />
-            Here you can access the dashboard functions available to you as an
-            organization.
-          </h2>
-          <div className="flex flex-grow items-center">
-            <div className="w-container grid w-full max-w-full grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-2">
-              <div className="flex flex-col gap-3  border-2 border-border bg-white p-5 shadow-light">
-                <h4 className="text-xl font-heading">Upload Document</h4>
-                <p>Upload your documents and get them converted to NFTs</p>
-              </div>
-              <div className="flex flex-col gap-3  border-2 border-border bg-white p-5 shadow-light">
-                <h4 className="text-xl font-heading">Records</h4>
-                <p>View your added records</p>
-              </div>
-              <div className="flex flex-col gap-3  border-2 border-border bg-white p-5 shadow-light">
-                <h4 className="text-xl font-heading">Notifications</h4>
-                <p>
-                  See the list of pending requests for access to your documents
-                </p>
-              </div>
-              <div className="flex flex-col gap-3  border-2 border-border bg-white p-5 shadow-light">
-                <h4 className="text-xl font-heading">Access Control</h4>
-                <p>
-                  See the documents which have been granted to organizations
-                </p>
+      <div className="inset-0 flex min-h-screen w-full flex-col items-center justify-between space-y-10 bg-white bg-[linear-gradient(to_right,#80808033_1px,transparent_1px),linear-gradient(to_bottom,#80808033_1px,transparent_1px)] bg-[size:70px_70px]">
+        <PageHeader title="Dashboard" />
+        <div className="flex w-full px-5">
+          <div className="flex w-full flex-col space-y-8 border-2 border-black bg-white p-8">
+            <h2 className="text-gray-700">
+              Welcome, <span className="font-semibold">{user!.username}</span>!
+              <br />
+              Here you can access the dashboard functions available to you as an
+              organization.
+            </h2>
+            <div className="flex items-center">
+              <div className="w-container grid w-full max-w-full grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-2">
+                {cards.map((card, i) => (
+                    <Link href={card.url} key={i}>
+                      <div
+                          className="flex -translate-x-1 -translate-y-1 items-center space-x-5 border-2 border-border bg-white px-5 py-5 shadow-light transition-all duration-200 hover:translate-x-0 hover:translate-y-0 hover:shadow-none active:translate-x-0 active:translate-y-0"
+                          key={i}
+                      >
+                        {card.icon}
+                        <div>
+                          <h4 className="text-xl font-heading">{card.title}</h4>
+                          <p>{card.description}</p>
+                        </div>
+                      </div>
+                    </Link>
+                ))}
               </div>
             </div>
           </div>
         </div>
+        <PageFooter />
       </div>
-      <PageFooter />
-    </div>
   );
 }
 
