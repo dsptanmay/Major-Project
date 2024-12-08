df74a52f469239f6b36349560841e1a8ae29729d
Tanmay Deshpande <dsp.tanmay@gmail.com>
2024-12-08 22:36:50 +0530

    made first section full screen

--- src/sections/header.tsx
+++ src/sections/header.tsx
@@ -5,22 +5,13 @@ import { useRouter } from "next/navigation";
 export default function Header() {
   const router = useRouter();
   return (
-    <header className="inset-0 flex min-h-[80dvh] w-full flex-col items-center justify-center bg-white bg-[linear-gradient(to_right,#80808033_1px,transparent_1px),linear-gradient(to_bottom,#80808033_1px,transparent_1px)] bg-[size:70px_70px]">
+    <header className="inset-0 flex min-h-screen w-full flex-col items-center justify-center bg-white bg-[linear-gradient(to_right,#80808033_1px,transparent_1px),linear-gradient(to_bottom,#80808033_1px,transparent_1px)] bg-[size:70px_70px]">
       <div className="w-container mx-auto max-w-full px-5 py-[110px] text-center lg:py-[150px]">
         <h1 className="text-3xl font-heading md:text-4xl lg:text-5xl">
           NFT Based Patient Health Records
         </h1>
         <p className="my-12 mt-8 text-lg font-normal leading-relaxed md:text-xl lg:text-2xl lg:leading-relaxed">
           Get started with your medical profile in minutes
-          {/* <br /> Check the{" "}
-          <a
-            target="_blank"
-            href="https://github.com/neobrutalism-templates/saas"
-            className="font-heading underline"
-          >
-            github repo
-          </a>{" "}
-          for more info. */}
         </p>
         <Button
           size="lg"
