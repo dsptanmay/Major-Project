55ef938d15f49105c479d29a3b9c0661e0fd94e0
Tanmay Deshpande <dsp.tanmay@gmail.com>
2024-12-10 03:13:28 +0530

    added base access control page

---
+++ src/app/(user)/access-control/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function AccessControlPage() {
+  return (
+    <div className="flex w-full max-w-6xl flex-col items-center bg-white p-5 shadow-light">
+      AccessControlPage
+    </div>
+  );
+}
+
+export default AccessControlPage;
