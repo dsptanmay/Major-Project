3bb99645f35eccda692997d1f6a1a903df26ac49
Tanmay Deshpande <dsp.tanmay@gmail.com>
2024-12-10 19:57:28 +0530

    added useEffect to fetch ipfs hash of token id

--- src/app/(user)/user_view/[token_id]/page.tsx
+++ src/app/(user)/user_view/[token_id]/page.tsx
@@ -1,12 +1,35 @@
 "use client";
+import { contract } from "@/app/client";
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Button } from "@/components/ui/button";
 import { Wallet } from "lucide-react";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { readContract } from "thirdweb";
 import { useActiveAccount } from "thirdweb/react";
 
 function UserViewPage({ params }: { params: { token_id: string } }) {
+  const [isFetched, setIsFetched] = useState<boolean>(false);
+  const [ipfsURL, setIpfsURL] = useState("");
   const activeAccount = useActiveAccount();
   const tokenId = params.token_id;
+
+  useEffect(() => {
+    const fetchData = async (token_id: bigint, caller: string) => {
+      const _uri = await readContract({
+        contract,
+        method:
+          "function getIPFSHash(uint256 tokenId, address caller) view returns (string)",
+        params: [token_id, caller],
+      });
+      if (_uri) {
+        setIpfsURL(
+          `https://${process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID}.ipfscdn.io/ipfs/${_uri.substring(7)}`,
+        );
+        setIsFetched(true);
+      }
+    };
+    if (activeAccount) fetchData(BigInt(tokenId), activeAccount.address);
+  }, [activeAccount, isFetched, tokenId]);
   if (!activeAccount)
     return (
       <div>
@@ -18,8 +41,9 @@ function UserViewPage({ params }: { params: { token_id: string } }) {
       </div>
     );
   return (
-    <div className="flex w-full max-w-6xl flex-col border-[3px] border-border bg-white p-5 shadow-light">
-      {tokenId}
+    <div className="flex w-full max-w-6xl flex-col space-y-5 border-[3px] border-border bg-white p-5 shadow-light">
+      <h1>Document for Token ID {tokenId}</h1>
+      {isFetched && <Button className="w-full">Decrypt Document</Button>}
     </div>
   );
 }
