6b5bbd555f4e7d75268c21c70a20f92c3dba1c12
Tanmay Deshpande <dsp.tanmay@gmail.com>
2024-12-11 02:02:59 +0530

    feat(user_view): added decrypt pdf function

--- src/app/(user)/user_view/[token_id]/page.tsx
+++ src/app/(user)/user_view/[token_id]/page.tsx
@@ -8,7 +8,7 @@ import { useActiveAccount } from "thirdweb/react";
 function UserViewPage({ params }: { params: { token_id: string } }) {
   const [isFetched, setIsFetched] = useState<boolean>(false);
   const [ipfsURL, setIpfsURL] = useState<string | null>(null);
-  const [encryptionKey, setEncryptionKey] = useState<string | null>(null);
+  const [decryptionKey, setDecryptionKey] = useState<string | null>(null);
   const activeAccount = useActiveAccount();
   const tokenId = params.token_id;
 
@@ -39,14 +39,83 @@ function UserViewPage({ params }: { params: { token_id: string } }) {
 
       if (tokenResponse.ok) {
         const data = await tokenResponse.json();
-        setEncryptionKey(data);
+        setDecryptionKey(data);
       } else {
-        setEncryptionKey(null);
+        setDecryptionKey(null);
       }
       setIsFetched(true);
     };
     if (activeAccount) fetchHash();
-  }, [activeAccount, tokenId, isFetched, ipfsURL]);
+  }, [activeAccount, tokenId, isFetched, ipfsURL, decryptionKey]);
+
+  const decryptPDF = async (
+    url: string,
+    decryptionKey: string,
+  ): Promise<{ pdfUrl: string; originalName: string }> => {
+    try {
+      const forge = await import("node-forge");
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`HTTP Error! Status: ${response.status}`);
+      }
+
+      const encryptedData = await response.json();
+
+      if (
+        !encryptedData.iv ||
+        !encryptedData.encryptedContent ||
+        !encryptedData.originalName
+      ) {
+        throw new Error("Invalid file formatting!");
+      }
+
+      if (decryptionKey.length === 0)
+        throw new Error("Decryption Key is empty!");
+
+      try {
+        const keyBytes = forge.util.hexToBytes(decryptionKey);
+        const ivBytes = forge.util.hexToBytes(encryptedData.iv);
+        const encryptedBytes = forge.util.hexToBytes(
+          encryptedData.encryptedContent,
+        );
+
+        const decipher = forge.cipher.createDecipher("AES-CBC", keyBytes);
+        decipher.start({
+          iv: ivBytes,
+        });
+
+        decipher.update(forge.util.createBuffer(encryptedBytes));
+        const pass = decipher.finish();
+        if (!pass) {
+          throw new Error("Decryption failed");
+        }
+
+        const decryptedBytes = decipher.output.getBytes();
+        if (decryptedBytes.length === 0)
+          throw new Error("Decrypted content is empty");
+
+        const base64PDF = btoa(decryptedBytes);
+
+        const pdfDataURL = `data:application/pdf;base64,${base64PDF}`;
+        return { pdfUrl: pdfDataURL, originalName: encryptedData.originalName };
+      } catch (conversionErr) {
+        console.error("Conversion Error:", conversionErr);
+        throw new Error(
+          `Conversion failed: ${
+            conversionErr instanceof Error
+              ? conversionErr.message
+              : "Unknown conversion error"
+          }`,
+        );
+      }
+    } catch (err) {
+      console.error("Decryption Error:", err);
+      throw new Error(
+        "Decryption failed: " +
+          (err instanceof Error ? err.message : "Unknown error"),
+      );
+    }
+  };
 
   if (!activeAccount)
     return (
@@ -59,7 +128,7 @@ function UserViewPage({ params }: { params: { token_id: string } }) {
       </div>
     );
 
-  if (!ipfsURL || !encryptionKey)
+  if (!ipfsURL || !decryptionKey)
     return (
       <div>
         <Alert className="bg-red-400">
