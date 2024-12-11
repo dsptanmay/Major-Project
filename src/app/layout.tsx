5c44ec67ad11b4f4888ceeb4b691dbae52b34f36
Tanmay Deshpande <dsp.tanmay@gmail.com>
2024-12-11 17:34:53 +0530

    fix(root): suppressing hydration warning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <html lang="en">
+      <html lang="en" suppressHydrationWarning>
         <body className={dmSans.className}>{children}</body>
       </html>
     </ClerkProvider>
