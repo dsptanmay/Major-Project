6d71e4374baf91238b02a6dbbfbecb24ad3a83d5
Tanmay Deshpande <dsp.tanmay@gmail.com>
2024-12-11 17:43:04 +0530

    feat(notifications_route): added POST method to api

--- src/app/api/notifications/route.ts
+++ src/app/api/notifications/route.ts
@@ -1,5 +1,5 @@
 import { db } from "@/db/drizzle";
-import { notificationsTable } from "@/db/schema";
+import { notificationsTable, organizationWalletTable } from "@/db/schema";
 import { eq } from "drizzle-orm";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -45,6 +45,61 @@ export async function GET(req: NextRequest) {
     return NextResponse.json(notifications, { status: 200 });
   } catch (err) {
     console.error(err);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 },
+    );
+  }
+}
+
+export async function POST(request: NextRequest) {
+  try {
+    const notificationData = await request.json();
+    const { org_address, org_name, nft_token_id, comments } = notificationData;
+
+    if (!org_address || !org_name || !nft_token_id || !comments)
+      return NextResponse.json(
+        { error: "Missing required fields" },
+        { status: 400 },
+      );
+
+    // Insert Organization's wallet address if not already existing
+    const existingOrg = await db
+      .select()
+      .from(organizationWalletTable)
+      .where(eq(organizationWalletTable.wallet_address, org_address));
+
+    if (existingOrg.length === 0)
+      await db
+        .insert(organizationWalletTable)
+        .values({ organization_name: org_name, wallet_address: org_address });
+
+    const userRecord = await db.query.userNFTsTable.findFirst({
+      where: (records, { eq }) => eq(records.token_id, nft_token_id),
+      columns: {
+        token_id: false,
+        description: false,
+        title: false,
+        user_address: true,
+      },
+    });
+    const user_address = userRecord!.user_address;
+    const newNotification = await db
+      .insert(notificationsTable)
+      .values({
+        org_address,
+        org_name,
+        user_address,
+        nft_token_id,
+        comments,
+        status: "pending",
+      })
+      .returning();
+
+    return NextResponse.json(newNotification[0], { status: 201 });
+  } catch (error) {
+    console.error("Error creating notification:", error);
+
     return NextResponse.json(
       { error: "Internal Server Error" },
       { status: 500 },
