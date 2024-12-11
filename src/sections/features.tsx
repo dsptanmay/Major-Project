f7f4444f0d42465d3bd98e4d9dbfe25d64735c87
Tanmay Deshpande <dsp.tanmay@gmail.com>
2024-12-11 17:34:27 +0530

    fix(home): fixed wrong layout of features

--- src/sections/features.tsx
+++ src/sections/features.tsx
@@ -27,7 +27,7 @@ function Features() {
   return (
     <div className="flex min-h-[60dvh] w-full flex-grow flex-col items-center justify-center space-y-20 border-t-[3px] border-t-border bg-white bg-[radial-gradient(#80808080_1px,transparent_1px)] px-10 py-20 [background-size:16px_16px] ">
       <h1 className="text-4xl font-bold">Key Features</h1>
-      <div className="grid gap-x-10 sm:grid-cols-1 sm:gap-y-10 sm:space-y-10 lg:grid-cols-3">
+      <div className="grid  gap-x-10 sm:grid-cols-1 lg:grid-cols-3">
         {features.map((feature, i) => (
           <Card
             className="w-[350px] -translate-x-1 -translate-y-1 transition-all duration-200 hover:translate-x-0 hover:translate-y-0 hover:shadow-none"
