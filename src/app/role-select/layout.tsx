abab13900c52338e307a43d5019ae52ede740a71
Tanmay Deshpande <dsp.tanmay@gmail.com>
2024-12-21 07:40:19 +0530

    refactor(role-select): added ThirdWebProvider

--- src/app/role-select/layout.tsx
+++ src/app/role-select/layout.tsx
@@ -3,6 +3,7 @@ import PageHeader from "@/components/page-header";
 import { ORG_ROLE, USER_ROLE } from "@/types/roles";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
+import { ThirdwebProvider } from "thirdweb/react";
 
 export default async function RoleSelectionLayout({
   children,
@@ -14,10 +15,12 @@ export default async function RoleSelectionLayout({
   const role = user.publicMetadata.role;
   if (role === USER_ROLE || role === ORG_ROLE) redirect("/dashboard");
   return (
-    <div className="flex min-h-screen w-full flex-col items-center justify-between border-2 border-border bg-white bg-[radial-gradient(#80808080_1px,transparent_1px)] shadow-light [background-size:16px_16px]">
-      <PageHeader title="Role Selection" />
-      {children}
-      <PageFooter />
-    </div>
+    <ThirdwebProvider>
+      <div className="flex min-h-screen w-full flex-col items-center justify-between border-2 border-border bg-white bg-[radial-gradient(#80808080_1px,transparent_1px)] shadow-light [background-size:16px_16px]">
+        <PageHeader title="Role Selection" />
+        {children}
+        <PageFooter />
+      </div>
+    </ThirdwebProvider>
   );
 }
