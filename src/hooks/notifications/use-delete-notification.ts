927571a94c62374b05f87824cc491cc9a8e203de
Tanmay Deshpande <dsp.tanmay@gmail.com>
2025-01-01 00:44:19 +0530

    feat(hooks): added delete notification hook

---
+++ src/hooks/notifications/use-delete-notification.ts
@@ -0,0 +1,47 @@
+import { useToast } from "@/hooks/use-toast";
+
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+
+import { apiClient } from "@/lib/hono";
+import { InferRequestType, InferResponseType } from "hono";
+
+type RequestType = InferRequestType<typeof apiClient.api.notifications.$delete>;
+type ResponseType = InferResponseType<
+  typeof apiClient.api.notifications.$delete,
+  201
+>;
+
+export const useDeleteNotification = (id?: string) => {
+  const queryClient = useQueryClient();
+  const { toast } = useToast();
+  const mutation = useMutation<ResponseType, Error, RequestType>({
+    mutationFn: async (notificationData) => {
+      const response = await apiClient.api.notifications.$delete({
+        ...notificationData,
+      });
+      if (!response.ok) throw new Error("Failed to delete notification");
+      const data = await response.json();
+      return data;
+    },
+
+    onSuccess: ({ data }) => {
+      toast({
+        title: "Success",
+        description: `Deleted notification successfully (${data.id})`,
+      });
+      queryClient.invalidateQueries({
+        queryKey: ["notifications", { id }],
+        exact: true,
+      });
+    },
+    onError: (err, variables) => {
+      console.error(err);
+      toast({
+        title: "Error",
+        description: `Failed to delete notification with ID ${variables.query.id}`,
+        variant: "destructive",
+      });
+    },
+  });
+  return mutation;
+};
