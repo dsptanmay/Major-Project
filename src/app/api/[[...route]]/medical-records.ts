28b32b631d483ed6cacba543593964e36b59d7b3
Tanmay Deshpande <dsp.tanmay@gmail.com>
2024-12-31 15:49:00 +0530

    feat(api): added medical records route

---
+++ src/app/api/[[...route]]/medical-records.ts
@@ -0,0 +1,66 @@
+import { Hono } from "hono";
+import { zValidator } from "@hono/zod-validator";
+import {
+  clerkMiddleware as clerkHonoMiddleware,
+  getAuth,
+} from "@hono/clerk-auth";
+
+import { insertRecordSchema, medicalRecords } from "@/db/schema";
+import { db } from "@/db/drizzle";
+import { z } from "zod";
+
+const medicalRecordsRouter = new Hono()
+  .use("*", clerkHonoMiddleware())
+  .get("/", async (c) => {
+    const auth = getAuth(c);
+    if (!auth?.userId) return c.json({ error: "Unauthorized" }, 401);
+
+    const records = await db.query.medicalRecords.findMany({
+      where: (record, { eq }) => eq(record.user_id, auth.userId),
+    });
+    return c.json({ records });
+  })
+  .get(
+    "/:token_id",
+    zValidator("param", z.object({ token_id: z.string().optional() })),
+    async (c) => {
+      const auth = getAuth(c);
+      const { token_id } = c.req.valid("param");
+      if (!auth?.userId) return c.json({ error: "Unauthorized" }, 401);
+      if (!token_id) return c.json({ error: "Token ID required" }, 401);
+
+      const record = await db.query.medicalRecords.findFirst({
+        where: (record, { eq }) => eq(record.token_id, token_id),
+      });
+      if (!record) return c.json({ error: "Record not found" }, 404);
+      return c.json({ record });
+    },
+  )
+  .post(
+    "/",
+    zValidator(
+      "json",
+      insertRecordSchema.omit({ id: true, uploaded_at: true, user_id: true }),
+    ),
+    async (c) => {
+      const auth = getAuth(c);
+      if (!auth?.userId) return c.json({ error: "Unauthorized" }, 401);
+      const data = c.req.valid("json");
+      try {
+        const newRecord = await db
+          .insert(medicalRecords)
+          .values({ ...data, user_id: auth.userId })
+          .returning();
+
+        if (newRecord.length === 0)
+          return c.json({ error: "Failed to insert record" }, 400);
+
+        return c.json(newRecord[0], 201);
+      } catch (error) {
+        console.error(error);
+        return c.json({ error: "Internal Server Error" }, 500);
+      }
+    },
+  );
+
+export default medicalRecordsRouter;
