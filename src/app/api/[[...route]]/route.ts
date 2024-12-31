3d5e03e4150e5c4c4c8590b1d035b32d1bd7b726
Tanmay Deshpande <dsp.tanmay@gmail.com>
2024-12-31 15:49:22 +0530

    refactor(api): added medical records route to routes.ts

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,12 +1,16 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
-import userRouter from "./users";
 
-// export const runtime = "edge";
+import userRouter from "./users";
+import medicalRecordsRouter from "./medical-records";
+
+export const runtime = "nodejs";
 
 const app = new Hono().basePath("/api");
 
-const routes = app.route("/users", userRouter);
+const routes = app
+  .route("/users", userRouter)
+  .route("/medical_records", medicalRecordsRouter);
 
 export const GET = handle(app);
 export const POST = handle(app);
