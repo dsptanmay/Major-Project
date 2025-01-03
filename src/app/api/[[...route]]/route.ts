56aee2802a007ac7a017e181e6fa179297cf61b1
Tanmay Deshpande <dsp.tanmay@gmail.com>
2025-01-03 15:47:12 +0530

    fix(api): removed edge runtime to see if build works

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -7,7 +7,7 @@ import notificationsRouter from "./notifications";
 import medicalRecordsRouter from "./medical-records";
 import accessRequestsRouter from "./access-requests";
 
-export const runtime = "edge";
+// export const runtime = "edge";
 
 const app = new Hono().basePath("/api").use("*", logger());
 
