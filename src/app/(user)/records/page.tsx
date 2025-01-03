b9a3ead5707a5fefee4e0559cf46cf2bd0effba7
Tanmay Deshpande <dsp.tanmay@gmail.com>
2025-01-03 10:55:35 +0530

    fix(records): using `AlertCard` for alerts now; removed unnecessary imports

--- src/app/(user)/records/page.tsx
+++ src/app/(user)/records/page.tsx
@@ -1,10 +1,14 @@
 "use client";
 import React from "react";
+import Link from "next/link";
+import { format } from "date-fns";
+import { useUser } from "@clerk/nextjs";
+
+import { AlertCircle } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import AlertCard from "@/components/alert-card";
 import LoadingStateComponent from "@/components/loading-card";
 import MissingWalletComponent from "@/components/missing-wallet";
-import { AlertCircle, CircleAlert, InfoIcon, Library } from "lucide-react";
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import {
   Card,
   CardContent,
@@ -13,17 +17,17 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 
-import Link from "next/link";
-
 import { useActiveAccount } from "thirdweb/react";
 import { useGetRecords } from "@/hooks/medical-records/use-get-records";
-import { format } from "date-fns";
-import { useUser } from "@clerk/nextjs";
 
 function RecordsPageContent() {
   const activeAccount = useActiveAccount();
   const { user } = useUser();
-  const { data: records, status, error } = useGetRecords(user?.id);
+  const {
+    data: records,
+    status,
+    error,
+  } = useGetRecords(user?.id, activeAccount?.address);
 
   if (!activeAccount) return <MissingWalletComponent />;
 
@@ -31,27 +35,22 @@ function RecordsPageContent() {
     return <LoadingStateComponent content="Loading records..." />;
   if (status === "error")
     return (
-      <div>
-        <Alert>
-          <AlertCircle className="h-4 w-4" />
-          <AlertTitle>Error</AlertTitle>
-          <AlertDescription>
-            Failed to fetch records. <br />
-            {error.message}
-          </AlertDescription>
-        </Alert>
-      </div>
+      <AlertCard
+        icon={<AlertCircle />}
+        title="Error"
+        description={error.message}
+        variant="error"
+      />
     );
 
   if (records.length === 0)
     return (
-      <div>
-        <Alert>
-          <Library className="h-4 w-4" />
-          <InfoIcon>Missing Records</InfoIcon>
-          <AlertDescription>No records found for user!</AlertDescription>
-        </Alert>
-      </div>
+      <AlertCard
+        icon={<AlertCircle />}
+        title="Error"
+        description="No Records found for user"
+        variant="status"
+      />
     );
   return (
     <div className="flex w-full max-w-6xl flex-col space-y-5 rounded-base border-2 border-border bg-white px-10 py-10 shadow-light ">
@@ -62,13 +61,6 @@ function RecordsPageContent() {
         </span>{" "}
         :
       </h2>
-      {error && (
-        <Alert className="bg-red-300">
-          <CircleAlert className="h-4 w-4" />
-          <AlertTitle>An Error Occurred!</AlertTitle>
-          <AlertDescription>{error}</AlertDescription>
-        </Alert>
-      )}
       <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
         {records.map((record, i) => (
           <Card key={i} className="bg-[#fff4e0]">
@@ -81,7 +73,7 @@ function RecordsPageContent() {
                 <h1>{format(record.uploaded_at, "dd MMM, yyyy")}</h1>
                 <h2>#{record.token_id}</h2>
               </div>
-              <Link href={`/view/${record.token_id}`} prefetch={true}>
+              <Link href={`/view/${record.token_id}`}>
                 <Button className="w-full bg-[#fd9745]">View Document</Button>
               </Link>
             </CardContent>
