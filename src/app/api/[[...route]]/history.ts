570e51cb13d0811c596b9059d135f55be779cd2b
Tanmay Deshpande <dsp.tanmay@gmail.com>
2025-01-06 15:24:43 +0530

    feat(history): added GET routes

--- src/app/api/[[...route]]/history.ts
+++ src/app/api/[[...route]]/history.ts
@@ -1,6 +1,8 @@
 import { db } from "@/db/drizzle";
+import { history, insertHistorySchema } from "@/db/schema";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { zValidator } from "@hono/zod-validator";
+import { and, eq } from "drizzle-orm";
 import { Hono } from "hono";
 import { z } from "zod";
 
@@ -16,15 +18,46 @@ const historyRouter = new Hono()
 
     return c.json({ records: historyRecords }, 200);
   })
-  .post(
-    "/",
-    zValidator("json", z.object({ comments: z.string() })),
-    async (c) => {
-      const auth = getAuth(c);
-      const data = c.req.valid("json");
-      if (!auth?.userId) return c.json({ error: "Unauthorized" }, 401);
-      return c.json({});
-    },
-  );
+  .get("/write", async (c) => {
+    const auth = getAuth(c);
+    if (!auth?.userId) return c.json({ error: "Unauthorized" }, 401);
+
+    const writeEvents = await db
+      .select({
+        id: history.id,
+        transaction_hash: history.transaction_hash,
+        description: history.comments,
+        performed_at: history.performed_at,
+      })
+      .from(history)
+      .where(
+        and(eq(history.user_id, auth.userId), eq(history.event_type, "write")),
+      );
+
+    if (!writeEvents.length)
+      return c.json({ error: "No write events found for user!" }, 404);
+
+    return c.json(writeEvents, 200);
+  })
+  .get("/read", async (c) => {
+    const auth = getAuth(c);
+    if (!auth?.userId) return c.json({ error: "Unauthorized" }, 401);
+
+    const readEvents = await db
+      .select({
+        id: history.id,
+        description: history.comments,
+        performed_at: history.performed_at,
+      })
+      .from(history)
+      .where(
+        and(eq(history.user_id, auth.userId), eq(history.event_type, "read")),
+      );
+
+    if (!readEvents.length)
+      return c.json({ error: "No write events found for user!" }, 404);
+
+    return c.json(readEvents, 200);
+  });
 
 export default historyRouter;
