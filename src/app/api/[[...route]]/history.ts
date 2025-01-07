5787b52b111e62ede5fb5f1c5287aa4182782c09
Tanmay Deshpande <dsp.tanmay@gmail.com>
2025-01-07 16:24:39 +0530

    fix(api): added schemas and orderBy clauses

--- src/app/api/[[...route]]/history.ts
+++ src/app/api/[[...route]]/history.ts
@@ -1,10 +1,20 @@
 import { db } from "@/db/drizzle";
-import { history, insertHistorySchema } from "@/db/schema";
+import { history } from "@/db/schema";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { zValidator } from "@hono/zod-validator";
 import { and, eq } from "drizzle-orm";
 import { Hono } from "hono";
 import { z } from "zod";
+
+const insertWriteEventSchema = z.object({
+  comments: z.string(),
+  transaction_hash: z.string(),
+});
+
+const insertReadEventSchema = z.object({
+  comments: z.string(),
+  token_id: z.string(),
+});
 
 const historyRouter = new Hono()
   .use("*", clerkMiddleware())
@@ -33,7 +43,8 @@ const historyRouter = new Hono()
       .where(
         and(eq(history.user_id, auth.userId), eq(history.event_type, "write")),
       )
-      .limit(10);
+      .limit(10)
+      .orderBy(history.performed_at);
 
     return c.json(writeEvents, 200);
   })
@@ -51,7 +62,8 @@ const historyRouter = new Hono()
       .where(
         and(eq(history.user_id, auth.userId), eq(history.event_type, "read")),
       )
-      .limit(10);
+      .limit(10)
+      .orderBy(history.performed_at);
 
     return c.json(readEvents, 200);
   })
@@ -86,7 +98,7 @@ const historyRouter = new Hono()
 
     return c.json(data, 200);
   })
-  .post("/write", zValidator("json", insertHistorySchema), async (c) => {
+  .post("/write", zValidator("json", insertWriteEventSchema), async (c) => {
     const auth = getAuth(c);
     if (!auth?.userId) return c.json({ error: "Unauthorized" }, 401);
 
@@ -107,24 +119,29 @@ const historyRouter = new Hono()
 
     return c.json(newEvent, 201);
   })
-  .post("/read", zValidator("json", insertHistorySchema), async (c) => {
+  .post("/read", zValidator("json", insertReadEventSchema), async (c) => {
     const auth = getAuth(c);
     if (!auth?.userId) return c.json({ error: "Unauthorized" }, 401);
 
     const data = c.req.valid("json");
+    const record = await db.query.medicalRecords.findFirst({
+      where: (record, { eq }) => eq(record.token_id, data.token_id),
+      columns: { user_id: true },
+    });
+
+    if (!record) return c.json({ error: "Record not found!" }, 404);
 
     const [newEvent] = await db
       .insert(history)
       .values({
         event_type: "read",
         comments: data.comments,
-        user_id: auth.userId,
+        user_id: record.user_id,
       })
       .returning();
 
     if (!newEvent)
       return c.json({ error: "Failed to create read event!" }, 500);
-
     return c.json(newEvent, 201);
   });
 
