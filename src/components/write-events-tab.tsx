ff21dbe614ea9bf763d8f009bbed1551d8a266df
Tanmay Deshpande <dsp.tanmay@gmail.com>
2025-01-07 15:03:15 +0530

    feat(tabs): added write events tab and removed shadow from the card

--- src/components/write-events-tab.tsx
+++ src/components/write-events-tab.tsx
@@ -22,36 +22,29 @@ import { format } from "date-fns";
 import { useUser } from "@clerk/nextjs";
 
 import { useQueryClient } from "@tanstack/react-query";
+import { useGetWriteEvents } from "@/hooks/history/use-get-write-events";
 
-import { useGetReadEvents } from "@/hooks/history/use-get-read-events";
-
-function ReadEventsTable({ userId }: { userId: string }) {
-  const { data, status } = useGetReadEvents(userId);
+function WriteEventsTable() {
+  const { user } = useUser();
+  const { data, status } = useGetWriteEvents(user?.id);
 
   if (status === "pending")
-    return (
-      <Skeleton className="w-full border-2 border-border px-5 py-10"></Skeleton>
-    );
+    return <Skeleton className="w-full border-2 border-border"></Skeleton>;
   if (status === "error") return <div>Error</div>; // TODO : replace with proper error component
-
-  if (data.length === 0)
-    return (
-      <div className="flex items-center justify-center border-2 border-border py-3 font-base">
-        No read events so far!
-      </div>
-    );
   return (
     <Table>
       <TableHeader>
         <TableRow>
-          <TableHead className="w-[70%]">Action</TableHead>
-          <TableHead className="w-[30%]">Performed At</TableHead>
+          <TableHead className="w-[45%]">Action</TableHead>
+          <TableHead className="w-[35%]">Transaction Hash</TableHead>
+          <TableHead className="w-[20%]">Performed At</TableHead>
         </TableRow>
       </TableHeader>
       <TableBody>
         {data.map((record) => (
           <TableRow key={record.id}>
             <TableCell>{record.description}</TableCell>
+            <TableCell>{record.transaction_hash}</TableCell>
             <TableCell>
               {format(new Date(record.performed_at), "HH:m a, do MMM")}
             </TableCell>
@@ -62,19 +55,19 @@ function ReadEventsTable({ userId }: { userId: string }) {
   );
 }
 
-function ReadEventsCard() {
+function WriteEventsCard() {
   const { user } = useUser();
   const queryClient = useQueryClient();
   return (
     <Card className="shadow-none">
       <CardHeader>
-        <CardTitle>Read Events</CardTitle>
+        <CardTitle>Write Events</CardTitle>
         <CardDescription>
-          A list of recent read events performed on your documents
+          A list of recent write events performed by you
         </CardDescription>
       </CardHeader>
       <CardContent>
-        <ReadEventsTable userId={user?.id!} />
+        <WriteEventsTable />
       </CardContent>
       <CardFooter className="flex w-full justify-end">
         <Button
@@ -82,7 +75,7 @@ function ReadEventsCard() {
           className="bg-[#FEF2E8] font-base"
           onClick={() => {
             queryClient.invalidateQueries({
-              queryKey: ["get-read-events", { id: user?.id }],
+              queryKey: ["get-write-events", { id: user?.id }],
             });
           }}
         >
@@ -93,4 +86,4 @@ function ReadEventsCard() {
   );
 }
 
-export default ReadEventsCard;
+export default WriteEventsCard;
